605e6e000677ce7e84503ed8d9a02e6bdf1f5bca
YogeshrajB <byogeshrajbe@gmail.com>
2025-08-07 11:01:05 +0530

    Add files via upload

---
+++ Prob1DSA.py
@@ -0,0 +1,12 @@
+'''Prob 1 : Write a program that takes an integer, then a string, then a char from the user and prints them in the screen.
+Input:  2 Name y
+Expected Output:
+2
+Name
+y'''
+num=int(input("enter the integer"))
+string=input("enter the string")
+char=input("enter the char")
+print(num)
+print(string)
+print(char)
